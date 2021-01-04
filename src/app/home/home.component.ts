a46a24302b2b81f8a530f37407ad560ec1051a0d
ecisneros <ecisneros@datastar.com.ar>
2021-01-04 16:46:47 -0300

    Primer prueba

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+ blogs = [];
+
+ ngOnInit() {
+   this.blogs = JSON.parse(localStorage.getItem("blogs"));
+   console.log(this.blogs); 
+ }
+
+ removeBlog(blog){
+   let index = this.blogs.indexOf(blog)
+   this.blogs.splice(index, 1)
+   localStorage.setItem("blogs",JSON.stringify(this.blogs))
+   alert("El Blog ha sido borrado")
+
+ }
+
+
+}
